e35cf20c4d63a3bcc9aa126f2f0c040447a47589
Aditya Jahdav <adityajadhav.kp@gmail.com>
2020-05-25 03:04:33 +0530

    Updates

---
+++ precache-manifest.79b09c5dc8bf2148deb7e4e864fdb2ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca80bc755dc7a02bb3e070d0e36483d",
+    "url": "/final/index.html"
+  },
+  {
+    "revision": "98ea0b14b05717995a74",
+    "url": "/final/static/css/main.ad196f34.chunk.css"
+  },
+  {
+    "revision": "7cca5ef8c953e1204f6b",
+    "url": "/final/static/js/2.ece9f532.chunk.js"
+  },
+  {
+    "revision": "98ea0b14b05717995a74",
+    "url": "/final/static/js/main.8dbc886c.chunk.js"
+  },
+  {
+    "revision": "e210fb4629529c34ca4e",
+    "url": "/final/static/js/runtime-main.9c2a56c7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/final/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/final/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/final/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/final/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/final/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/final/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/final/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/final/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
